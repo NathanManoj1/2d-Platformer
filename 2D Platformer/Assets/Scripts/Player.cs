447aafa01d76e1ddce613da4886126baf39c17f4
Snezel <74683422+Snezel@users.noreply.github.com>
2021-03-26 10:15:45 -0400

    Implemented Player Movement
    Player can now jump and move.

---
+++ 2D Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 _direction;
+    [SerializeField]
+    private float _speed = 10;
+    [SerializeField]
+    private float _jumpForce = 10;
+    private bool _isGrounded = false;
+    [SerializeField]
+    private Transform _isGroundedChecker;
+    [SerializeField]
+    private float _checkGroundRadius = 5;
+    [SerializeField]
+    private LayerMask _groundLayer;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * _speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, _jumpForce);
+        }
+    }
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(_isGroundedChecker.position, _checkGroundRadius, _groundLayer);
+        if (collider != null)
+        {
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
+    }
+}
